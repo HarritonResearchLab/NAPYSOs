22f532f80b9affda7ae49447cf00351f49bd32a1
thissop <68932815+thissop@users.noreply.github.com>
2021-08-06 21:01:07 -0700

    Create calculate.py

---
+++ calculate.py
@@ -0,0 +1,13 @@
+def Q(dates, mags, magerrs, timescale): 
+    '''
+    dates: array of time values associated with observations; e.g. array of mjd's
+    mags: array of magnitude values associated with observations
+    magerrs: array of errors associated with magnitudes in mags array
+    timescale: float to calculate Q off
+    '''
+    
+def M(mags):
+    '''
+    mags: array of magnitude values 
+    '''
+    
